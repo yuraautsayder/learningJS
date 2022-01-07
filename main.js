6b649d23199b8b159513ecc39a9c4d388f033032
Yuriy <meresin.y@outlook.com>
2022-01-07 14:05:40 +0400

    test if else

---
+++ main.js
@@ -0,0 +1,10 @@
+const time = 12;
+
+if(time < 12){
+    console.log('dobroe utro');
+} else if(time >= 12 && time < 18) {
+    console.log('dobriy den')
+} else {
+    console.log ('dobriy vecher')
+}
+
