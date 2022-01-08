b6082d2c09f1093ac3d5a13b28e56ac4972d9435
Yuriy <meresin.y@outlook.com>
2022-01-08 14:46:22 +0400

    test functions

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const time = 12;
-
-if(time < 12){
-    console.log('dobroe utro');
-} else if(time >= 12 && time < 18) {
-    console.log('dobriy den')
-} else {
-    console.log ('dobriy vecher')
+function sayHi(userName) {
+    console.log(`Привет, ${userName}! Как твои дела?`);
 }
 
+sayHi('huesos')
+sayHi('raz')
+sayHi('dva')
+sayHi('tri')
