4b2bb28ac6571eddc08323f84fb5e51d02adaff6
Yuriy <meresin.y@outlook.com>
2022-01-09 16:19:50 +0400

    test funkcii

--- main.js
+++ main.js
@@ -2,8 +2,15 @@
 //     const result = a + b;
 // }
 
+
+
 // const summ = (a, b) => {
 //     return a + b;
 // };
 
-// const summ = (a, b) => a + b;
+
+
+const summ = (a, b) => a + b;
+
+const res = summ(10, 30);
+console.log (res)
