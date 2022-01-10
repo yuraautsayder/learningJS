faca1cd77f410b71a97661e05b8bcde78be8eecc
Yuriy <meresin.y@outlook.com>
2022-01-10 21:47:05 +0400

    ob'ekti

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-// циклы
+// объекты 
 
-// for (let i = 0; i < 5; i++) {
-//     console.log(i);
-// }
+let userName = 'mark';
+let age = 24;
+let isMarried = false;
 
-// console.log('Finish')
+const person = {
+    userName: 'Ivan',
+    age: 24,
+    isMarried: false,
 
-//обход массива циклом for 
+}
 
-const autoBrands = ['test1', 'test2', 'test3', 'test4',];
-
-
-// for (let item of autoBrands) {
-//     console.log(item)
-// }
-
-// obhod massiv forEach()
-
-autoBrands.forEach(function(item, index){
-    console.log(`${item} => ${index}`);
-})
+console.log(person)
+console.log(person.userName)
+console.log(person['age'])
