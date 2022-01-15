1a00da8a7114961a91bd5af92d0e3c84e82662a0
Yuriy <meresin.y@outlook.com>
2022-01-16 02:13:49 +0400

    классы. конструкторы объектов

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// segodnya chisto rabochiy kod. push dlya otmetki
+// классы. контрукторы объектов 
+
+class Person {
+    constructor(userName, age, isMarried) {
+        this.userName = userName;
+        this.age = age;
+        this.isMarried = isMarried;
+    }
+
+    sayHi(name) {
+        console.log(`privet, ${name}! poshel nahuy ot ${this.userName}`)
+    }
+}
+
+const person1 = new Person('mark', 30, false);
+const person2 = new Person('troy', 30, true);
+
+console.log(person1)
+console.log(person2)
+person1.sayHi('ahuel')
